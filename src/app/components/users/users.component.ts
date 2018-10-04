95c5c53bfeffe4c6095a190b138989226d23b399
Vishesh <visheshsingh16@gmail.com>
2018-10-04 16:31:46 -0400

    Looping with ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { User } from "../../models/User";
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.css"]
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  constructor() {}
 
   ngOnInit() {
+    this.users = [
+      {
+        firstName: "Mark",
+        lastName: "Anthony",
+        age: 31,
+        address: {
+          street: "3 Horizon lane",
+          city: "Dallas",
+          state: "TX"
+        }
+      },
+      {
+        firstName: "Jessica",
+        lastName: "Pearson",
+        age: 37,
+        address: {
+          street: "BlueFox Avenue",
+          city: "Princeton",
+          state: "NJ"
+        }
+      },
+      {
+        firstName: "Harvey",
+        lastName: "Spectre",
+        age: 24,
+        address: {
+          street: "1240 Greenbelt Blvd",
+          city: "Cincinnati",
+          state: "OH"
+        }
+      }
+    ];
   }
-
 }
